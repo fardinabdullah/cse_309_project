d880304b903c649f441ab4811e16d9ff5ae3ed55
Fardin Abdullah <fardinabdullah22nov@gmail.com>
2026-07-22 22:54:52 +0600

    Improve workspace dashboard UI and frontend integration

---
+++ frontend/src/api/workspaceApi.js
@@ -0,0 +1,58 @@
+import axios from "axios";
+
+
+// Backend API URL
+const API_URL = "http://127.0.0.1:8000/workspaces";
+
+
+// Get all workspaces
+export const getWorkspaces = async () => {
+
+    return await axios.get(`${API_URL}/`);
+
+};
+
+
+// Create a new workspace
+export const createWorkspace = async (workspaceData) => {
+
+    return await axios.post(
+        `${API_URL}/`,
+        workspaceData
+    );
+
+};
+
+
+// Get single workspace
+export const getWorkspaceById = async (id) => {
+
+    return await axios.get(
+        `${API_URL}/${id}`
+    );
+
+};
+
+
+// Update workspace
+export const updateWorkspace = async (
+    id,
+    workspaceData
+) => {
+
+    return await axios.put(
+        `${API_URL}/${id}`,
+        workspaceData
+    );
+
+};
+
+
+// Delete workspace
+export const deleteWorkspace = async (id) => {
+
+    return await axios.delete(
+        `${API_URL}/${id}`
+    );
+
+};
